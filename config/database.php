6d7deb937f2581bea452bf404fa2d996c03aa89e
thanhhtth1807034 <thanhhtth1807034@fpt.edu.vn>
2019-09-10 09:07:05 +0700

    doi pass database

--- config/database.php
+++ config/database.php
@@ -70,7 +70,7 @@ return [
             'port' => '5432',
             'database' => 'zero-waste',
             'username' => 'postgres',
-            'password' => 'Bach@2111',
+            'password' => 'abcD1234',
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
