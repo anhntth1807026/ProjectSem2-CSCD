3b1d202b5b11bd9cc04acae1b80401a405efe876
luongxuanbach <43542436+luongxuanbach@users.noreply.github.com>
2019-09-14 15:35:36 +0700

    update

--- resources/views/client/layout.blade.php
+++ resources/views/client/layout.blade.php
@@ -25,7 +25,6 @@
     <link rel="stylesheet" href="{{ asset('css/w3CSS.css') }}">
 
     {{--    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">--}}
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
     <link rel="stylesheet" href="{{ asset('css/style-client.css') }}">
 
@@ -35,6 +34,7 @@
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('css/materialize.min.css') }}">
 
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
 </head>
 <body>
