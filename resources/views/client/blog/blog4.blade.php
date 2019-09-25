2c82adaca48eaa9b57f5e08e28f2eeac42dbab00
Dang Tieu Binh <binhsaveup@gmail.com>
2019-09-25 16:31:47 +0700

    fix blog and blogdetail fault style

--- resources/views/client/blog/blog4.blade.php
+++ resources/views/client/blog/blog4.blade.php
@@ -445,121 +445,16 @@
     </div>
 
 
-    <h3 style="text-align: center">BÌNH LUẬN</h3>
-
     <ul class="comment-section">
-
-        <li class="comment user-comment">
-
-            <div class="info">
-                <a href="#">Anie Silverston</a>
-                <span>4 hours ago</span>
-            </div>
-
-            <a class="avatar" href="#">
-                <img src="{{ asset('video/avatar_user_1.jpg') }}" width="35" alt="Profile Avatar"
-                     title="Anie Silverston"/>
-            </a>
-
-            <p>Suspendisse gravida sem?</p>
-
-        </li>
-
-        <li class="comment user-comment">
-
-            <div class="info">
-                <a href="#">Jack Smith</a>
-                <span>3 hours ago</span>
-            </div>
-
-            <a class="avatar" href="#">
-                <img src="{{ asset('video/avatar_user_2.jpg') }}" width="35" alt="Profile Avatar" title="Jack Smith"/>
-            </a>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse gravida sem sit amet molestie
-                portitor.</p>
-
-        </li>
-
-        <li class="comment user-comment">
-
-            <div class="info">
-                <a href="#">Bradley Jones</a>
-                <span>1 hour ago</span>
-            </div>
-
-            <a class="avatar" href="#">
-                <img src="{{ asset('video/avatar_user_1.jpg') }}" width="35" alt="Profile Avatar"
-                     title="Bradley Jones"/>
-            </a>
-
-            <p>Suspendisse gravida sem sit amet molestie portitor?</p>
-
-        </li>
-
-        <li class="comment author-comment">
-
-            <div class="info">
-                <a href="#">Jack Smith</a>
-                <span>1 hour ago</span>
-            </div>
-
-            <a class="avatar" href="#">
-                <img src="{{ asset('video/avatar_author.jpg') }}" width="35" alt="Profile Avatar" title="Jack Smith"/>
-            </a>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisee gravida sem sit amet molestie
-                porttitor.</p>
-
-        </li>
-
-        <li class="comment author-comment">
-
-            <div class="info">
-                <a href="#">Jack Smith</a>
-                <span>1 hour ago</span>
-            </div>
-
-            <a class="avatar" href="#">
-                <img src="{{ asset('video/avatar_user_1.jpg') }}" width="35" alt="Profile Avatar" title="Jack Smith"/>
-            </a>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisee gravida sem sit amet molestie
-                porttitor.</p>
-
-        </li>
-
-        <li class="comment author-comment">
-
-            <div class="info">
-                <a href="#">Jack Smith</a>
-                <span>1 hour ago</span>
-            </div>
-
-            <a class="avatar" href="#">
-                <img src="{{ asset('video/avatar_user_1.jpg') }}" width="35" alt="Profile Avatar" title="Jack Smith"/>
-            </a>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisee gravida sem sit amet molestie
-                porttitor.</p>
-
-        </li>
-
         <li class="write-new">
-
             <form action="#" method="post">
-
                 <textarea placeholder="Write your comment here" name="comment"></textarea>
-
                 <div>
                     <img src="{{ asset('video/avatar_user_1.jpg') }}" width="35" alt="Profile of Bradley Jones"
                          title="Bradley Jones"/>
                     <button type="submit">Submit</button>
                 </div>
-
             </form>
-
         </li>
-
     </ul>
 @endsection
